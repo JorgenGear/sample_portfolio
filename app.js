cc7780ec153d86fc5678cd17c89cdb1725e97e44
Rhett Jorgensen <121641690+JorgenGear@users.noreply.github.com>
2024-08-28 11:38:31 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import ProjectList from './ProjectList';
+import About from './About';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<ProjectList />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
