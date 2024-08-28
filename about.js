d8a59aea528115d2beef510a8a06c81069b7ec08
Rhett Jorgensen <121641690+JorgenGear@users.noreply.github.com>
2024-08-28 11:36:06 -0600

    Create about.js

---
+++ about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is where you can describe yourself and your background.</p>
+    </div>
+  );
+}
+
+export default About;
